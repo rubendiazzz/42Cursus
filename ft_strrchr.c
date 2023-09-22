edca5e6a13c4675d068c81951561142c8b727d45
Ruben Díaz <ruubendiazz@Rubens-MacBook-Air.local>
2023-09-22 13:30:34 +0200

    a

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rdiaz-fr <rdiaz-fr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 14:50:18 by rdiaz-fr          #+#    #+#             */
-/*   Updated: 2023/09/22 13:26:49 by rdiaz-fr         ###   ########.fr       */
+/*   Updated: 2023/09/22 13:30:24 by rdiaz-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,9 @@ char	*ft_strrchr(const char *s, int c)
 
 	if (s == NULL)
 		return (NULL);
+	i = ft_strlen(s);
 	if (c == '\0')
-		return ((char *)(s + ft_strlen(s)));
-	i = 0;
-	while (s[i])
-		i++;
+		return ((char *)(s + i));
 	while (i >= 0)
 	{
 		if (s[i] == (char)c)
