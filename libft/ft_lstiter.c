59d7321eff1f185f5d9e9012c16d19e17eb705cf
Ruben Díaz <ruubendiazz@Rubens-MacBook-Air.local>
2023-09-25 17:48:40 +0200

    Done

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: rdiaz-fr <rdiaz-fr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 19:40:42 by rdiaz-fr          #+#    #+#             */
-/*   Updated: 2023/09/24 19:41:32 by rdiaz-fr         ###   ########.fr       */
+/*   Updated: 2023/09/25 17:48:13 by rdiaz-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	while ((*lst).next)
+	if (!lst || !f)
+		return ;
+	while (lst)
 	{
-		f((*lst).content);
-		lst = (*lst).next;
+		f(lst->content);
+		lst = lst->next;
 	}
-	f((*lst).content);
 }
