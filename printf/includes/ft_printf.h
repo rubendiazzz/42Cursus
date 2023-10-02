8e0af7110b3d68cd046538cbe75bb339bea8075d
Rubén Díaz Fraga <rubendfraga@gmail.com>
2023-10-02 10:42:04 +0200

    Added structures
    Added structures in printf.h and called them initialized on 0 in printf.c

--- printf/includes/ft_printf.h
+++ printf/includes/ft_printf.h
@@ -3,6 +3,22 @@
 
 # include "../libft/libft.h"
 # include <stdarg.h>
+# include <unistd.h>
+
+typedef struct s_print                       
+{                        
+      va_list  args;    // arg to print out                    
+      int   wdt;        // width                
+      int   prc;        // precision              
+      int   zero;       // zero padding               
+      int   pt;         // .     
+      int   dash;       // -               
+      int   tlngth;     // total_length (return value)              
+      int   sign;       // pos or neg number               
+      int   is_zero;    // is number zero                 
+      int   perc;       // %               
+      int   space;      // space flag ' '            
+}    t_print;
 
 int	ft_printf(const	char *, ...);
 
--- printf/src/ft_printf.c
+++ printf/src/ft_printf.c
@@ -10,10 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft/libft.h"
 #include "../includes/libftprintf.h"
 
-int	ft_printf(const	char *, ...)
+t_print *ft_initialise_tab(t_print *tab)                       
+{                       
+      tab->wdt = 0;        // we set everything to 0, false        
+      tab->prc = 0;                        
+      tab->zero = 0;                        
+      tab->pt = 0;                        
+      tab->sign = 0;                        
+      tab->tlngth = 0;                        
+      tab->is_zero = 0;                        
+      tab->dash = 0;                        
+      tab->perc = 0;                        
+      tab->space = 0;                        
+      return (tab);                       
+}
+int ft_printf(const char *format, ...)
 {
-	
+   t_print *tab;
+                       
+   tab = (t_print *)malloc(sizeof(t_print));                        
+   if (!tab)                         
+       return (-1);
+   ft_initialise_tab(tab)
 }
