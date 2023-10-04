df6a5e8857d7e3ce972b048d5fba8e345bbda98e
Rubén Díaz Fraga <rubendfraga@gmail.com>
2023-10-04 11:33:14 +0200

    Teletrabajo
    trabajo de printf hecho desde casa

--- printf/includes/ft_printf.h
+++ printf/includes/ft_printf.h
@@ -1,25 +1,37 @@
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdiaz-fr <rdiaz-fr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 15:08:48 by rdiaz-fr          #+#    #+#             */
+/*   Updated: 2023/10/03 15:10:14 by rdiaz-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include "../libft/libft.h"
 # include <stdarg.h>
 # include <unistd.h>
 
-typedef struct s_print                       
-{                        
-      va_list  args;    // arg to print out                    
-      int   wdt;        // width                
-      int   prc;        // precision              
-      int   zero;       // zero padding               
-      int   pt;         // .     
-      int   dash;       // -               
-      int   tlngth;     // total_length (return value)              
-      int   sign;       // pos or neg number               
-      int   is_zero;    // is number zero                 
-      int   perc;       // %               
-      int   space;      // space flag ' '            
-}    t_print;
+typedef struct s_print
+{
+	va_list	args;
+	int		wdt;
+	int		prc;
+	int		zero;
+	int		pt;
+	int		dash;
+	int		tlngth;
+	int		sign;
+	int		is_zero;
+	int		perc;
+	int		space;
+}	t_print;
 
-int	ft_printf(const	char *, ...);
+int	ft_printf(const char *format, ...);
 
 #endif
--- printf/src/ft_printf.c
+++ printf/src/ft_printf.c
@@ -6,33 +6,42 @@
 /*   By: rdiaz-fr <rdiaz-fr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 13:15:42 by rdiaz-fr          #+#    #+#             */
-/*   Updated: 2023/10/01 16:33:03 by rdiaz-fr         ###   ########.fr       */
+/*   Updated: 2023/10/03 18:20:12 by rdiaz-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/ft_printf.h"
 #include "../libft/libft.h"
-#include "../includes/libftprintf.h"
 
-t_print *ft_initialise_tab(t_print *tab)                       
-{                       
-      tab->wdt = 0;        // we set everything to 0, false        
-      tab->prc = 0;                        
-      tab->zero = 0;                        
-      tab->pt = 0;                        
-      tab->sign = 0;                        
-      tab->tlngth = 0;                        
-      tab->is_zero = 0;                        
-      tab->dash = 0;                        
-      tab->perc = 0;                        
-      tab->space = 0;                        
-      return (tab);                       
-}
-int ft_printf(const char *format, ...)
+int	ft_printchar(int c)
 {
-   t_print *tab;
-                       
-   tab = (t_print *)malloc(sizeof(t_print));                        
-   if (!tab)                         
-       return (-1);
-   ft_initialise_tab(tab)
+	write(1, &c, 1);
+	return (1);
+}
+
+t_print	*ft_initialise_tab(t_print *tab)
+{
+	tab->wdt = 0;
+	tab->prc = 0;
+	tab->zero = 0;
+	tab->pt = 0;
+	tab->sign = 0;
+	tab->tlngth = 0;
+	tab->is_zero = 0;
+	tab->dash = 0;
+	tab->perc = 0;
+	tab->space = 0;
+	return (tab);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int		i;
+	int		ret;
+	t_print	*tab;
+
+	tab = (t_print *)malloc(sizeof(t_print));
+	if (!tab)
+		return (-1);
+	ft_initialise_tab(tab);
 }
