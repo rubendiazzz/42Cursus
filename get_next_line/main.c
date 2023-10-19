b179cc469be1d372f95bf62b89343c53cf86c9bb
Rubén Díaz Fraga <rubendfraga@gmail.com>
2023-10-19 15:50:08 +0200

    Nuevo main
    Cambios en el main

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -6,24 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rdiaz-fr <rdiaz-fr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/19 10:49:29 by rdiaz-fr          #+#    #+#             */
-/*   Updated: 2023/10/19 10:50:48 by rdiaz-fr         ###   ########.fr       */
+/*   Created: 2023/10/19 15:48:21 by rdiaz-fr          #+#    #+#             */
+/*   Updated: 2023/10/19 15:49:59 by rdiaz-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int	main(void)
 {
-	char	*line;
 	int		fd;
+	char	*line;
 
 	fd = open("test.txt", O_RDONLY);
 	if (fd == -1)
 	{
-		printf("Failed to open file\n");
+		printf("Error: Could not open file\n");
 		return (1);
 	}
 	while ((line = get_next_line(fd)) != NULL)
@@ -33,39 +33,4 @@ int	main(void)
 	}
 	close(fd);
 	return (0);
-=======
-#include "get_next_line.h"
-#include <stdio.h>
-#include <fcntl.h>
-
-int main(int argc, char **argv)
-{
-    int fd;
-    char *line;
-
-    if (argc != 2)
-    {
-        printf("Usage: %s <filename>\n", argv[0]);
-        return (1);
-    }
-
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-    {
-        perror("Error");
-        return (1);
-    }
-
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("Output: %s\n", line);
-        free(line);
-    }
-
-    if (line == NULL)
-        printf("EOF reached or an error occurred\n");
-
-    close(fd);
-    return (0);
->>>>>>> 834a993bdd3323c686b52e9c8e97ea7421ab2207
 }
